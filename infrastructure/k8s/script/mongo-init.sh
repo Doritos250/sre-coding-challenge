488b0477f300b34bf1feea2d1e02fb7b771581fd
root <root@DESKTOP-L51E8TD.localdomain>
2019-03-09 19:17:52 +0100

    Adapt Pipeline to perform rolling update remove MultiVAC legacy folder Change mongo docker image provider

--- infrastructure/k8s/script/mongo-init.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-kubectl exec -it mongo-0 -- mongo --eval 'rs.initiate()'
-sleep 3s
-kubectl exec -it mongo-0 -- mongo multivac --eval 'db.createUser( { user: "multivac", pwd: "g84C7F3ZtMtfNgqD", roles: [ { role: "readWrite", db: "multivac" } ] } )'
